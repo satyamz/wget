d93c1c3f8ce7571ebcecce232bb94e434559925f
Satyam Zode <satyamzode@gmail.com>
2015-07-03 10:31:51 +0530

    Add new Test Case for recursive download *testenv/Test-ftp-r.py : New file for recursive download *testenv/Test-ftp.py : Retain original

--- testenv/Test-ftp-r.py
+++ testenv/Test-ftp-r.py
@@ -5,15 +5,21 @@ from test.base_test import FTP
 from misc.wget_file import WgetFile
 
 """
-    This test ensures that Wget can download files from FTP Server
+    This test ensures that Wget recursive download.
 """
 TEST_NAME = "FTP Downloads"
 ########## File Definitions ######################################
 File1 = "Beyond the fog lies clarity"
+
+# Define directory structure by creating WgetFile object.
+# Here URL path is /Foo/File1
+# hence creating Foo and Bar is directory
+# Make the flag isdir=True and add parent
+
 A_File = WgetFile("File1", File1, parent="/Foo")
 
-WGET_OPTIONS = " -S "
-WGET_URLS = [["File1"]]
+WGET_OPTIONS = " -r "
+WGET_URLS = [["/Foo/File1"]]
 
 Files = [[A_File]]
 
@@ -44,3 +50,4 @@ err = FTPTest(
 ).begin()
 
 exit(err)
+
